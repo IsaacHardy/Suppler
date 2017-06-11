f5ed71090a65504a852302091e9503f7b9f40b74
Isaac Hardy <isaac@ihardy.co>
2017-06-11 19:16:24 -0400

    placed supplementals onto screen

--- public/main.js
+++ public/main.js
@@ -1,5 +1,42 @@
-var el = document.querySelector('.container');
+var refreshBtn = document.querySelector('#refreshBtn'),
+    myHeaders = new Headers(),
+    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpc2FhYy5oYXJkeUB0aGVpcm9ueWFyZC5jb20iLCJrZXkiOiIxZGViZDkzZWZmN2YyMzczNjUxNjdiNGJhNzYwMDI2MCJ9.yBv0xgmmfdzdQ2vjGRsJcPozvDQeJ2pUAXfRCfIOHg4',
+    newlineURL = 'https://newline.theironyard.com/api';
 
-el.addEventListener("click", function() {
-  getAllSupplementals();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer " + token);
+
+var get = {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default'
+};
+
+var post = {
+  method: 'POST',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default'
+};
+
+refreshBtn.addEventListener("click", function() {
+  fetch(newlineURL + '/supplementals', get)
+    .then(function(res) {
+      return res.json().then(function(json) {
+        console.log(json);
+        for (var i = 0; i < json.data.length; i++) {
+          var list = document.querySelector('.list');
+          var divChild = document.createElement('div');
+
+          divChild.innerHTML =
+            json.data[i].id + " - " + 
+            json.data[i].title + " - " +
+            json.data[i].description;
+
+          list.appendChild(divChild);
+        }
+      })
+
+    });
 });
