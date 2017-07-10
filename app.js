73ed11ab2cf94483fc47210ff157efca7a2bdc85
Isaac Hardy <isaac@ihardy.co>
2017-07-10 13:53:43 -0400

    Updated suppler with new functionality: Heroku ready, MVC, login support, and copy to clipboard support

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var express = require('express'),
-    http = require('http');
+const express          = require('express');
+const http             = require('http');
+const bodyParser       = require('body-parser');
+const validator        = require('express-validator');
 
-var app = express();
+const mustacheExpress  = require("mustache-express");
+const session          = require("express-session");
+const routes           = require("./routes");
+const path             = require("path");
 
-var allowCrossDomain = function(req, res, next) {
+const app              = express();
+
+const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -17,22 +24,23 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
-// configure
-app.configure(function(){
-  app.use(allowCrossDomain);
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.static('public'))
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-
-// dev config
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// Middlewares and things
+app.use(allowCrossDomain);
+app.set('port', process.env.PORT || 3000);
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.set('layout', 'layout');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(validator());
+app.use(session({
+  secret: 'thisisasecret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(routes);
 
 // run the server
 http.createServer(app).listen(app.get('port'), function(){
---
+++ routes/index.js
@@ -0,0 +1,108 @@
+const express    = require('express');
+const router     = express.Router();
+const request    = require('request');
+const cp         = require("copy-paste");
+const newlineURL = 'https://newline.theironyard.com/api';
+
+router.get("/", function(req, res) {
+  if (req.session.token) {
+    res.redirect("/supplementals");
+  } else {
+    res.render("login");
+  }
+});
+
+router.get("/supplementals", function(req, res) {
+  let options = {
+    method: "GET",
+    uri: newlineURL + "/supplementals",
+    auth: {
+      'bearer': req.session.token
+    },
+    header: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  };
+
+  if (req.session.token) {
+    request(options,
+      function (error, response, body) {
+        if (!error) {
+          res.render("index", {supplers: JSON.parse(body).data, copied: req.session.copied, title: req.session.title});
+        }
+      });
+  } else {
+    res.redirect("/");
+  }
+});
+router.get("/supplementals/:id", function(req, res){
+  let options = {
+    method: "GET",
+    uri: newlineURL + "/supplementals" + "/" + req.params.id,
+    auth: {
+      'bearer': req.session.token
+    },
+    header: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  };
+
+  if (req.session.token) {
+    request(options,
+      function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          cp.copy(JSON.parse(body).body, function() {
+            req.session.copied = true;
+            req.session.title = JSON.parse(body).title;
+            res.redirect("/supplementals");
+          });
+        } else {
+          req.session.title = "There was an error. Please try again.";
+          res.redirect("/supplementals");
+        }
+      });
+  } else {
+    res.redirect("/supplementals");
+  }
+});
+router.post("/", function(req, res) {
+  let form = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let options = {
+    method: "POST",
+    uri: newlineURL + "/auth",
+    form: form,
+    header: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  };
+  request(options,
+    function (error, response, body) {
+      if (!error && response.statusCode !== 401) {
+        req.session.token = JSON.parse(body).jwt;
+
+        res.setHeader('Content-Type', 'application/json');
+
+        res.redirect("/");
+      } else {
+        res.render("login", {error: "You entered an incorrect username and password combination. Please try again."});
+      }
+    });
+});
+
+router.get("/logout", function(req, res) {
+  req.session.destroy(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+module.exports = router;
